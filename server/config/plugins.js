d4740fb9412ec564db415ccc4310c2d5449a65d1
Pedro Matos de Carvalho <pedromatosdecarvalho@gmail.com>
2014-07-06 22:40:25 +0100

    goodOptions plugin disabled because it was launching an uncaught error... Creating issue for future improvement

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,19 +1,23 @@
 module.exports = function(server) {
     // Options to pass into the 'Good' plugin
+    /* not working..
     var goodOptions = {
         subscribers: {
             console: ['ops', 'request', 'log', 'error'],
             'tmp/logs/': ['ops', 'request', 'log', 'error']
         }
     };
+    */
     // The Assets Configuration Options
     var assetOptions = require('../../assets');
 
     server.pack.register([
+        /* not working..
         {
             plugin: require("good"),
             options: goodOptions
         },
+        */
         {
             plugin: require("hapi-assets"),
             options: assetOptions
