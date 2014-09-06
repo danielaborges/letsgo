8e1eaa2df1938e2ab48fb66c5d2951d5e3edce35
Pedro Matos de Carvalho <pedromatosdecarvalho@gmail.com>
2014-09-06 17:56:45 +0100

    bower.json

--- public/partials/EventData/EventDataCtrl.js
+++ public/partials/EventData/EventDataCtrl.js
@@ -12,7 +12,45 @@ appControllers.controller('EventDataCtrl', ['$scope', '$routeParams', 'EventFact
             $scope.eventData =  result.data.eventData;
         });
     }
-
+    
+    $scope.form = {
+        'formStructure' : [
+            {
+                'formGroupNgClass' : '{ "has-error" : userNameForm.userName.$invalid && !userNameForm.userName.$pristine }',
+                'ngFormName' : 'userNameForm',
+                'labelFor' : 'userName',
+                'labelClass' : 'col-md-2 control-label',
+                'labelText' : 'Nome',
+                'inputType' : 'text',
+                'inputId' : 'userName',
+                //'inputValidate' : '',
+                'inputName' : 'userName', 
+                'inputPlaceHolder' : 'Nome Contribuinte',
+                'inputClass' : 'form-control col-md-offset-1',
+                'inputNgModel' : 'user.name',
+                'otherAttributes' : 'required',
+                'errorNgShow' : 'userFieldForm.userName.$invalid && !userFieldForm.userName.$pristine',
+                'errorText' : 'Campo obrigatório'
+            }/*,
+            {
+                'formGroupNgClass' : '{ "has-error" : userEmailForm.userEmail.$invalid && !userEmailForm.userEmail.$pristine }',
+                'ngFormName' : 'userEmailForm',
+                'labelFor' : 'userEmail',
+                'labelClass' : 'col-md-2 control-label',
+                'labelText' : 'Email',
+                'inputType' : 'email',
+                'inputId' : 'userEmail',
+                //'inputValidate' : '',
+                'inputName' : 'userEmail', 
+                'inputPlaceHolder' : 'myEmail@emailHost.com',
+                'inputClass' : 'form-control col-md-offset-1',
+                'inputNgModel' : 'user.email',
+                'otherAttributes' : 'required',
+                'errorNgShow' : 'userFieldForm.userEmail.$invalid && !userFieldForm.userEmail.$pristine',
+                'errorText' : 'Email inválido'
+            }*/
+        ]
+    }
 
     initialize();
 
